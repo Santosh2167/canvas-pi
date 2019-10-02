975b28929b9d042ad894a66e87a8077420d89fe9
karuna <santosh.poudyal@gmail.com>
2019-10-02 15:48:56 +1000

    finished passing function from canvas to canvas controller

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 import ColourSelector from "./ColourSelector";
 
 export default class Canvas extends Component {
+  state = { hex: "#0000ff", width: 400, height: 400 };
+
+
+
+  onColourSelectorChange = (hex) => {
+    this.setState({ hex })
+  }
+
   render() {
+    const { hex, width, height } = this.state;
     return (
-      <ColourSelector />
+      <div>
+        <div>
+          <ColourSelector hex={hex} onColourSelectorChange={this.onColourSelectorChange} />
+        </div>
+        <canvas
+          width={width}
+          height={height}
+          style={{ border: "2px solid black" }}
+        />
+      </div>
     )
   }
 }
--- src/ColourSelector.js
+++ src/ColourSelector.js
@@ -2,23 +2,22 @@ import React, { Component } from "react";
 
 class ColourSelector extends Component {
 
-  state = { hex: this.props.hex };
-
-  onInputChange(event) {
-    this.setState({ hex: event.target.value })
+  onInputChange = (event) => {
+    const { onColourSelectorChange } = this.props;
+    onColourSelectorChange(event.target.value);
   }
+  // componentDidUpdate() {
+  //   console.log(this.state.hex);
+  // }
 
-  componentDidUpdate() {
-    console.log(this.state.hex);
-  }
   render() {
-    const { hex } = this.state;
+    const { hex } = this.props;
     return (
       <div>
         <input
           type="color"
           value={hex}
-          onChange={event => this.onInputChange(event)} />
+          onChange={this.onInputChange} />
       </div>
     );
   }
